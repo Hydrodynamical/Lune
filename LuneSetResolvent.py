f0cdde434b1c56fd685097f0b50a32227c9dd756
JKM <joseph.kent.miller@gmail.com>
2024-10-07 15:15:42 -0700

    modified:   LuneSetResolvent.py

--- LuneSetResolvent.py
+++ LuneSetResolvent.py
@@ -1,51 +1,42 @@
 import numpy as np              # arrays
 import matplotlib.pyplot as plt # plotting
 from tqdm import tqdm           # loading bar
-import concurrent.futures       # parallel computing
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
 
 def is_in_lune_set(p, k, N):
     """Check if p \in R^2 is in the lune set 
     L(k, N) = \{p : |p| > N, |p - k| \leq N \}.
-    
-    Inputs:
-        p: np.array
-        k: np.array
-        N: int
-        
-    Returns:
-        bool
     """
-    if (np.linalg.norm(p) > N) and (np.linalg.norm(p - k) <= N):
-        return True
-    else:
-        return False
-    
-# # Test function is_in_lune_set
-# N = 10
-# test_p = np.array([3,4])
-# test_k = np.array([-10,-1])
-# print(is_in_lune_set(test_p, test_k))
+    return (np.linalg.norm(p) > N) and (np.linalg.norm(p - k) <= N)
 
 def generate_lune_set(k, N):
     """Generate the lune set as a list of np.arrays.
     """
     cutoff = N + np.max(np.abs(k))
     lune_set = []
+    count = 0
     for i in range(-cutoff, cutoff + 1, 1):
         for j in range(-cutoff, cutoff + 1, 1):
             p = np.array([i,j])
             if is_in_lune_set(p, k, N):
                 lune_set.append(p)
-    return lune_set
+                count += 1
+    return lune_set, count
 
-# # Test generate lune set
-# N = 10
-# test_k = np.array([-10,-1])
-# print(generate_lune_set(test_k, N))
+def count_lune_set(k,N):
+    count = 0
+    cutoff = N + np.max(np.abs(k))
+    for i in range(-cutoff, cutoff + 1, 1):
+        for j in range(-cutoff, cutoff + 1, 1):
+            p = np.array([i,j])
+            if is_in_lune_set(p, k, N):
+                count+=1
+    return count
 
 def LuneResolvent(k, N):
     result = 0.0
-    lune_set = generate_lune_set(k, N)
+    lune_set, _ = generate_lune_set(k, N)
     for p in lune_set:
         result += 1/(np.linalg.norm(p) ** 2 - np.linalg.norm(k - p) ** 2)
     return result
@@ -54,20 +45,25 @@ def LuneResolvent(k, N):
 # returns 1.5707564620188683
 
 
-k=np.array([5, 7])
-lune_set_list = [generate_lune_set(k, N = n) for n in range(100, 500, 10)]
-
-# Parallelizing the computation
-with concurrent.futures.ProcessPoolExecutor() as executor:
-    results = list(executor.map(LuneResolvent, lune_set_list))
-
-print(results)
-
-
 # Plot the function
-x = np.arange(1000, 5000, 100)
-y = [LuneResolvent(k, N=n) for n in tqdm(x, desc="Calculating LuneResolvent")]
-plt.plot(x, y)
+k = np.array([5,7])
+
+def calculate_resolvent(n):
+    return LuneResolvent(k, N=n)
+
+x = np.arange(100, 5000, 50)
+
+# Use ProcessPoolExecutor for parallel computation
+with ProcessPoolExecutor() as executor:
+    # Submit tasks to the executor and wrap them in tqdm for progress tracking
+    futures = [executor.submit(calculate_resolvent, n) for n in x]
+    
+    # Use as_completed to process the results as they finish
+    results = []
+    for future in tqdm(as_completed(futures), total=len(x), desc="Calculating LuneResolvent"):
+        results.append(future.result())
+
+plt.plot(x, results)
 plt.xlabel('Fermi Radius N')
 plt.ylabel('S(k, N)')
 plt.show()
